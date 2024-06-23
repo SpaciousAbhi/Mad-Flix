214ddf53b8fa6c1f0bbb26475cb55d24bac7e0da
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2024-06-23 14:01:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 #main variables
 API_ID = int(environ.get('API_ID', '4770590'))
 API_HASH = environ.get('API_HASH', 'e33bf9032335b874acb9c6406f044836')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7339126123:AAHoEjz1Zot614xoaJJpWkLP9__IhYiALlE')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7339126123:AAHneNmeqC5NzTfFX1C-th1HD4il_ffSd1A')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1557374187').split()]
 USERNAME = environ.get('USERNAME', "https://telegram.me/IAmVenomStone")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002224824058'))
