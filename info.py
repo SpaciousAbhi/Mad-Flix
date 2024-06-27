968724e63c45db8ef76cad39ac86d5964e9a70d0
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2024-06-27 10:38:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ request_channel = environ.get('REQUEST_CHANNEL', '-1002241752271')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
 UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002113810572'))
 # bot settings
-IS_PM_SEARCH = is_enabled('IS_PM_SEARCH', False)
+IS_PM_SEARCH = is_enabled('IS_PM_SEARCH', True)
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
 PORT = os.environ.get('PORT', '8080')
 MAX_BTN = int(environ.get('MAX_BTN', '8'))
