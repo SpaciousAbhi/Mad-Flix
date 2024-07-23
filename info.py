c41a9db6c38123360444754c47bc798e4296ce42
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2024-07-23 12:02:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002113810572'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/VenomStoneNetwork')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002038220265 -1002026830023').split()]
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://biyerax927:5RRqhXAbDR2P8luy@cluster0.lv1shat.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://miwiy36931:RYPIy355MF7jOHyC@cluster0.lx6krxw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://memoba3523:nHdXCyvAFyWgV3mU@cluster0.1f4ctns.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'cluster0')
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002113810572'))
