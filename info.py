5955f09b155f8b7645ac03c0a786cf0b0e807041
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2024-07-27 00:18:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ PUBLICEARN = 'publicearn.com'
 SHRINKFOREARN = 'shrinkforearn.in'
 ADRINOLINKS = 'adrinolinks.in'
 ZIPSHORT = 'zipshort.net'
-STICKERS_IDS = ('CAACAgQAAxkBAAEK99dlfC7LDqnuwtGRkIoacot_dGC4zQACbg8AAuHqsVDaMQeY6CcRojME').split()
+STICKERS_IDS = ('').split()
 
 # verify system vars
 IS_VERIFY = is_enabled('IS_VERIFY', True)
